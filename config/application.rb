93a6f469b99890cb90940aff3e6031080540634d
Eddie Kao <eddie@digik.com.tw>
2016-09-29 17:35:20 +0800

    update config

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -8,8 +7,12 @@ Bundler.require(*Rails.groups)
 
 module HappyHacking
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.generators do |g|
+      g.stylesheets     false
+      g.javascripts     false
+      g.test_framework  false
+      g.helper          false
+    end
   end
 end
+
